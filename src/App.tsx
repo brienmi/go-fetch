5c54ed00d771dd2955c44f2c0ee64072b81e32b3
Brien M <brienmi87@gmail.com>
2023-09-28 14:36:16 -0500

    created boilerplate tools and set up css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
-import Message from './Message';
+import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
-  return <div><Message /></div>;
+  const [alertVisibile, setAlertVisibility] = useState(false);
+
+  let items = [
+    "Brussels Griffon",
+    "Redbone Hound",
+    "Great Dane",
+    "Labrador Retriever",
+    "Great Pyrenees",
+  ];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  return (
+    <div>
+      {alertVisibile && (
+        <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>
+      )}
+      <Button color="primary" onClick={() => setAlertVisibility(true)}>
+        My Button
+      </Button>
+      <ListGroup
+        items={items}
+        heading="Breeds"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
 }
 
 export default App;
